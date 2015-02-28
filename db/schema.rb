531aa6bb4531c8f5db636205814f6c304c204e9c
jarzab920813 <jarzab920813@wp.pl>
2015-02-28 14:11:46 +0100

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,6 @@
 ActiveRecord::Schema.define(version: 20150228111702) do
 
 
-
-
   create_table "users", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.string   "email",      limit: 255
